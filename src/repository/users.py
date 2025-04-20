8f04c0e45b58671d5fdab0d7e5de8bbbad1de2e6
Darina Khalina <darina.khalina@namecheap.com>
2025-04-20 14:32:25 +0300

    add docs for users repo

--- src/repository/users.py
+++ src/repository/users.py
@@ -11,18 +11,55 @@ class UserRepository:
         self.db = session
 
     async def get_user_by_id(self, user_id: int) -> User | None:
+        """
+        Get a User by their ID.
+
+        Args:
+            user_id (int): The ID of the User to retrieve.
+
+        Returns:
+            User | None: The User object if found, otherwise None.
+        """
         user = await self.db.execute(select(User).filter(User.id == user_id))
         return user.scalar_one_or_none()
 
     async def get_user_by_username(self, username: str) -> User | None:
+        """
+        Get a User by their username.
+
+        Args:
+            username (str): The username of the user to retrieve.
+
+        Returns:
+            User | None: The User object if found, otherwise None.
+        """
         user = await self.db.execute(select(User).filter(User.username == username))
         return user.scalar_one_or_none()
 
     async def get_user_by_email(self, email: str) -> User | None:
+        """
+        Get a User by their email address.
+
+        Args:
+            email (str): The email of the user to retrieve.
+
+        Returns:
+            User | None: The User object if found, otherwise None.
+        """
         user = await self.db.execute(select(User).filter(User.email == email))
         return user.scalar_one_or_none()
 
     async def create_user(self, body: UserCreate, avatar: Optional[str] = None) -> User:
+        """
+        Create a new User and save them to the database.
+
+        Args:
+            body (UserCreate): The User data to be used for creating the new user.
+            avatar (Optional[str], optional): The avatar URL for the user (default is None).
+
+        Returns:
+            User: The created User object with their details, including the avatar if provided.
+        """
         new_user = User(**body.model_dump(exclude_unset=True), avatar=avatar)
         self.db.add(new_user)
         await self.db.commit()
@@ -30,6 +67,17 @@ class UserRepository:
         return new_user
 
     async def update_avatar_url(self, email: str, url: str) -> User | None:
+        """
+        Update the avatar URL for a user identified by their email.
+
+        Args:
+            email (str): The email of the user whose avatar URL is to be updated.
+            url (str): The new avatar URL to be set for the user.
+
+        Returns:
+            User | None: The updated user object if the user exists and the avatar URL is updated,
+            or None if the user does not exist.
+        """
         user = await self.get_user_by_email(email)
 
         if user:
@@ -39,6 +87,15 @@ class UserRepository:
             return user
 
     async def verify_email(self, email: str) -> None:
+        """
+        Verify the email of a user by updating the `confirmed` field to `True`.
+
+        Args:
+            email (str): The email of the user to be verified.
+
+        Returns:
+            None
+        """
         user = await self.get_user_by_email(email)
 
         if user:
@@ -46,6 +103,16 @@ class UserRepository:
             await self.db.commit()
 
     async def update_user(self, user: User, body: UserUpdate) -> bool:
+        """
+        Update the details of an existing User in the database.
+
+        Args:
+            user (User): The user to be updated.
+            body (UserUpdate): The data to update the user with.
+
+        Returns:
+            bool: Returns `True` if the update was successful.
+        """
         for key, value in body.model_dump(exclude_unset=True).items():
             setattr(user, key, value)
         await self.db.commit()
